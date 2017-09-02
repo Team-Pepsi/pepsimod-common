a1b64f90ad862d436ac933e1afd2d4b9f13e2e58
DaPorkchop_ <joey.rabil@gmail.com>
2017-09-03 00:58:14 +0200

    better encryption

--- src/main/java/team/pepsi/pepsimod/common/util/CryptUtils.java
+++ src/main/java/team/pepsi/pepsimod/common/util/CryptUtils.java
@@ -15,37 +15,21 @@
 
 package team.pepsi.pepsimod.common.util;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.Security;
+import org.springframework.security.crypto.encrypt.BytesEncryptor;
+import org.springframework.security.crypto.encrypt.Encryptors;
 
 public class CryptUtils {
-    public static Cipher cipher;
+    public static String salt = "salt!";
 
-    static {
-        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-        try {
-            cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
-        } catch (Exception e)   {
-            e.printStackTrace();
-            Runtime.getRuntime().exit(9237432);
-        }
-    }
-
-    public static byte[] encrypt(String string, String password)    {
+    public static byte[] encrypt(String string, String password) {
         return encrypt(string.getBytes(), password);
     }
 
-    public static byte[] encrypt(byte[] input, String password)  {
+    public static byte[] encrypt(byte[] input, String password) {
         try {
-            byte[] keyBytes = password.getBytes();
-            SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            byte[] cipherText = new byte[cipher.getOutputSize(input.length)];
-            int ctLength = cipher.update(input, 0, input.length, cipherText, 0);
-            cipher.doFinal(cipherText, ctLength);
-            return cipherText;
-        } catch (Exception e)   {
+            BytesEncryptor encryptor = Encryptors.standard(password, salt);
+            return encryptor.encrypt(input);
+        } catch (Exception e) {
             e.printStackTrace();
             Runtime.getRuntime().exit(98473);
         }
@@ -55,14 +39,9 @@ public class CryptUtils {
 
     public static byte[] decrypt(byte[] input, String password) {
         try {
-            byte[] keyBytes = password.getBytes();
-            SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            byte[] plainText = new byte[cipher.getOutputSize(input.length)];
-            int ptLength = cipher.update(input, 0, input.length, plainText, 0);
-            cipher.doFinal(plainText, ptLength);
-            return plainText;
-        } catch (Exception e)   {
+            BytesEncryptor decryptor = Encryptors.standard(password, salt);
+            return decryptor.decrypt(input);
+        } catch (Exception e) {
             e.printStackTrace();
             Runtime.getRuntime().exit(40932);
         }
